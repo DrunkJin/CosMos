05b3cc11f4a0a420afbc5c47ba451196157b1eed
DrunkJin <ckae930413@gmail.com>
2022-06-15 22:45:01 +0900

    seojin_b2845 풀이

---
+++ 220612-220618/b2845/seojin_b2845.py
@@ -0,0 +1,12 @@
+l, p = map(int, input().split())
+news = list(map(int, input().split()))
+
+people = l*p
+
+news = list(map(lambda x : x - people, news))
+
+result = ''
+for i in news:
+    result += str(i)+" "
+    
+print(result)
