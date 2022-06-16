daf6d51f4dce545a247ce633eb2412dc99471a05
DrunkJin <ckae930413@gmail.com>
2022-06-16 21:35:43 +0900

    add DrunkJin_b2163 plus

--- 220612-220618/b2163/DrunkJin_b2163.py
+++ 220612-220618/b2163/DrunkJin_b2163.py
@@ -3,3 +3,22 @@ n, m = map(int, input().split())
 # n * m ÀÇ »ç°¢Çü n-1¹ø ÂÉ°³¸é n°³°¡ »ı±è. n(m-1)¹ø ÂÉ°³¸é m°³°¡ ´õ ÂÉ°³Áü
 
 print(n*m - 1)
+
+'''
+# ¿Ö Æ²·ÈÀ»±î?
+count = 0
+def split_choco(N, count):
+    while (N != 1):
+        N //= 2
+        count += 1
+    return count
+
+if n == m == 1:
+    pass
+else:
+    count += 1
+    count = split_choco(n, count)
+    count = split_choco(m, count)
+    
+print(count)
+'''
