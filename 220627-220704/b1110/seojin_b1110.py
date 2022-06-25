84f2648b1739e3a54dffb4e491cd9a1baf4833a4
DrunkJin <ckae930413@gmail.com>
2022-06-25 13:36:50 +0900

    test

--- 220627-220704/b1110/seojin_b1110.py
+++ 220627-220704/b1110/seojin_b1110.py
@@ -7,7 +7,7 @@ target = number
 count = 0
 
 while True:
-    if number == target & count != 0:
+    if number == target and count != 0:
         print(count)
         break
 
@@ -15,7 +15,7 @@ while True:
         if number > 9:
             ten_digit = number // 10
             one_digit = number % 10
-            number = (one_digit*10) + (ten_digit + one_digit)//10
+            number = (one_digit*10) + (ten_digit + one_digit)%10
             count += 1
         else:
             number *= 10
