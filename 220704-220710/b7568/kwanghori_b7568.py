90f81547a05c9690282e3884d93d1ef967d6d6b2
LEE KWANGHO <85268213+brain4652@users.noreply.github.com>
2022-07-04 22:00:19 +0900

    Add kwanghori_b7568.py

---
+++ 220704-220710/b7568/kwanghori_b7568.py
@@ -0,0 +1,26 @@
+class example:
+
+    def __init__(self, array) -> None:
+        self.array = array
+
+    def sol(self):
+        records = ()    # 등수를 저장
+        for tup_1 in self.array:    #등수를 판별할 데이터
+            k = 1
+            for tup_2 in self.array:    # 비교 데이터
+                if tup_1 == tup_2: pass  # 자기자신이므로 넘어감
+                elif tup_1[0] < tup_2[0] and tup_1[1] < tup_2[1]:   # 비교 데이터가 키와 몸무게 모두 크다면 +1등
+                    k += 1
+                else: pass
+            
+            records.append(str(k))
+
+        return print(' '.join(records))
+
+if __name__ == '__main__':
+    physical_record = []
+    for _ in range(int(input())):
+        a,b = input().split()
+        physical_record.append((int(a),int(b)))     # 몸무게와 키를 튜플로 받아 리스트에 저장 ex) [(weight1, height1), ...]
+    b7568 = example(physical_record)
+    b7568.sol()
