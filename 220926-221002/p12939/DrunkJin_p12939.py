94f5fc32b24063ad20110b0f3f97bfa922a48454
SeoJin <84702722+DrunkJin@users.noreply.github.com>
2022-09-26 15:06:11 +0900

    add DrunkJin_p12939.py

---
+++ 220926-221002/p12939/DrunkJin_p12939.py
@@ -0,0 +1,18 @@
+# 문제 설명
+# 문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+# 예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+#
+# 제한 조건
+# s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+# s1 = "1 2 3 4"
+# s2 = "-1 -2 -3 -4"
+
+def solution(s):
+    s_l = s.split(" ")
+    s_l_int = list(map(int, s_l))
+    answer = ''
+    max_number = str(max(s_l_int))
+    min_number = str(min(s_l_int))
+
+    answer += min_number+" "+max_number
+    return answer
